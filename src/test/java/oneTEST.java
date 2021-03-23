b2b48c319d8d8167c80eacae0473b4f233a0bdd2
Mails2018 <dileytes@gmail.com>
2021-03-23 16:24:13 +0200

    multi-threaded

--- src/test/java/oneTEST.java
+++ src/test/java/oneTEST.java
@@ -2,35 +2,37 @@ import allure.CustomAllureLifecycle;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Epic("EpicEpic")
 @Feature("FeatureFeature")
 public class oneTEST {
 
-    Steps steps;
-
     @BeforeSuite(alwaysRun = true)
     public void setListener() {
         Allure.setLifecycle(CustomAllureLifecycle.getInstance(Allure.getLifecycle()));
     }
 
-    @BeforeMethod
-    public void startTest() {
-        steps = new Steps();
-    }
+//    @BeforeMethod
+//    public void startTest() {
+//        steps = new Steps();
+//    }
 
-    @Test(description = "first test description")
-    public void test() {
+    @Test(description = "first test description", dataProvider = "soft assert")
+    public void test(Steps steps) {
+        long id = Thread.currentThread().getId();
+        System.out.println("first test thread " + id);
         steps.stepTwo("'StepTwo with true'", true);
         steps.stepOne("'StepOne'");
         steps.assertAll();
     }
 
-    @Test(description = "second test description")
-    public void test2() {
+    @Test(description = "second test description", dataProvider = "soft assert")
+    public void test2(Steps steps) {
+        long id = Thread.currentThread().getId();
+        System.out.println("second test thread " + id);
         steps.stepTwo("'StepTwo with false'", false);
         steps.stepOne("'StepOne'");
         steps.stepFour("'StepFour'");
@@ -39,10 +41,17 @@ public class oneTEST {
         steps.assertAll();
     }
 
-    @Test(description = "third test description")
-    public void test3() {
+    @Test(description = "third test description", dataProvider = "soft assert")
+    public void test3(Steps steps) {
+        long id = Thread.currentThread().getId();
+        System.out.println("third test thread " + id);
         steps.failTest();
         steps.stepThree("'StepThree'");
         steps.assertAll();
+    }
+
+    @DataProvider(name = "soft assert")
+    public Object[][] newSoftAssert() {
+        return new Object[][]{{new Steps()}};
     }
 }
